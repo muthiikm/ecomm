a06c49669f7a7fcc559774f79680134004846286
muthiikm <muthiikm@gmail.com>
2024-03-20 16:19:14 +0300

    security-configuration of jwt utilities

---
+++ Downloads/ecom/src/main/java/com/elp/ecom/utils/JwtUtl.java
@@ -0,0 +1,35 @@
+package com.elp.ecom.utils;
+
+import org.springframework.stereotype.Component;
+import sun.jvm.hotspot.debugger.cdbg.Sym;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+
+public class JwtUtl {
+    // This is a secret key. You should keep it secure and not expose it in your code.
+    public static final  String SECRET ="2345678900000000987654567890";
+    public String generateToken(String userName){
+
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims , userName);
+    }
+    private String createToken(Map<String,Object> claims, String userName){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpirationDate(System.currentTimeMillis()+1000*60*30)
+                .signWith(getSignKey().SignatureAlgorithm.HS256).compact();
+
+    }
+    private Key getSignKey(){
+
+        byte[]keybytes = Decoders.BASE64.decode(SECRET);
+    }
+}
