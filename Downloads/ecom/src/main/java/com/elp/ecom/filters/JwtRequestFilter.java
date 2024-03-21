96756f0b2f58cf48238efcf8c26eb7af75e73023
muthiikm <muthiikm@gmail.com>
2024-03-21 16:49:51 +0300

    updating User and token validation

--- Downloads/ecom/src/main/java/com/elp/ecom/filters/JwtRequestFilter.java
+++ Downloads/ecom/src/main/java/com/elp/ecom/filters/JwtRequestFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -35,14 +36,15 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             username = jwtUtl.extractUserName(token);
         }
         //validate user
-if(username != null && SecurityContextHolder.getContext().getAuthentication()==null);
+if(username != null && SecurityContextHolder.getContext().getAuthentication()==null){
     UserDetails userDetails = userDetailsService.loadUserByUsername(username);
     //validate token
-        if(jwtUtl.validateToken(token, userDetails)){
-            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null );
+        if(jwtUtl.validateToken(token, userDetails)) {
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null);
             authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authToken);
-
         }
+        }
+filterChain.doFilter(request, response);
     }
 }
